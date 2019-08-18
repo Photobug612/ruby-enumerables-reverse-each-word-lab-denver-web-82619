105a8cbc9ed99028c3c9befb624d9aaa77fc857c
Arlene George <aemg@live.com>
2019-08-18 22:07:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string = string.split("")
   reversed_words = []
-  reversed_words = string.collect {|word| woord.reverse
+  reversed_words = string.collect {|word| word.reverse}
   end
 end
   
